b036e727927d2f1e43c549b518a96a4a105c57b7
Max Daunarovich <max.daunarovich@gmail.com>
2020-10-23 00:24:26 +0300

    Remove button from bottom bars

--- src/components/ScriptBottomBar.tsx
+++ src/components/ScriptBottomBar.tsx
@@ -1,57 +1,14 @@
-import React, { useState } from "react";
-import {
-  useSetExecutionResultsMutation,
-  ResultType
-} from "../api/apollo/generated/graphql";
+import React from "react";
+import { ResultType } from "api/apollo/generated/graphql";
 
-import { FaArrowCircleRight } from "react-icons/fa";
-import Button from "./Button";
-import { useProject } from "providers/Project/projectHooks";
 import { RenderResponse } from "components/RenderResponse";
 import { Feedback as FeedbackRoot } from "layout/Feedback";
 import { Heading } from "layout/Heading";
-import { FeedbackActions } from "layout/FeedbackActions";
 import { ClearResults } from "./TransactionBottomBar";
 
 const ScriptBottomBar: React.FC = () => {
-  const { createScriptExecution, isSavingCode, project, active } = useProject();
-
-  const [setResult] = useSetExecutionResultsMutation();
-  const [executingScript, setExecutingScript] = useState(false);
-
   return (
     <FeedbackRoot>
-      <FeedbackActions>
-        <Button
-          onClick={async () => {
-            if (!executingScript) {
-              setExecutingScript(true);
-
-              let rawResult;
-              try {
-                rawResult = await createScriptExecution();
-              } catch (e) {
-                console.error(e)
-                rawResult = e.toString();
-              }
-
-              setExecutingScript(false);
-              setResult({
-                variables: {
-                  resultType: ResultType.Script,
-                  rawResult,
-                  label: project.scriptTemplates[active.index].title
-                }
-              });
-            }
-          }}
-          disabled={isSavingCode}
-          isLoading={executingScript}
-          Icon={FaArrowCircleRight}
-        >
-          Execute
-        </Button>
-      </FeedbackActions>
       <Heading>
         Script Results <ClearResults type={ResultType.Script} />
       </Heading>
