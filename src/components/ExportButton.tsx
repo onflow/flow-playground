970211315d4e8e6e82c97b17b455a120677b7c11
Max Daunarovich <max.daunarovich@gmail.com>
2020-11-17 15:32:19 +0300

    Prettify

--- src/components/ExportButton.tsx
+++ src/components/ExportButton.tsx
@@ -1,30 +1,30 @@
-import React from "react";
-import useClipboard from "react-use-clipboard"
-import {FaClipboard, FaClipboardCheck} from "react-icons/fa";
-import {SidebarItemExport} from "layout/SidebarItemExport";
-import {useProject} from "providers/Project/projectHooks";
-import {generateCode} from "../util/generate-code";
+import React from 'react';
+import useClipboard from 'react-use-clipboard';
+import { FaClipboard, FaClipboardCheck } from 'react-icons/fa';
+import { SidebarItemExport } from 'layout/SidebarItemExport';
+import { useProject } from 'providers/Project/projectHooks';
+import { generateCode } from '../util/generate-code';
 
 type ExportButtonProps = {
-  id: string,
-  typeName: string
-}
+  id: string;
+  typeName: string;
+};
 
 export const ExportButton = (props: ExportButtonProps) => {
-  const { project } = useProject()
-  const {id, typeName} = props
-  const code = generateCode(project.id, id, typeName)
+  const { project } = useProject();
+  const { id, typeName } = props;
+  const code = generateCode(project.id, id, typeName);
   const [isCopied, setCopied] = useClipboard(code, {
-    successDuration: 1000
+    successDuration: 1000,
   });
 
-  return(
-      project.id === "LOCAL-project"
-        ? null
-        : (
-          <SidebarItemExport onClick={setCopied} title={"Copy snippet to clipboard"} active={isCopied}>
-          {isCopied ? <FaClipboardCheck/> : <FaClipboard/>}
-        </SidebarItemExport>
-        )
-  )
-}
+  return project.id === 'LOCAL-project' ? null : (
+    <SidebarItemExport
+      onClick={setCopied}
+      title={'Copy snippet to clipboard'}
+      active={isCopied}
+    >
+      {isCopied ? <FaClipboardCheck /> : <FaClipboard />}
+    </SidebarItemExport>
+  );
+};
--- src/layout/SidebarItemDelete.tsx
+++ src/layout/SidebarItemDelete.tsx
@@ -1,25 +1,25 @@
-import React from "react"
-import styled from "@emotion/styled"
+import React from 'react';
+import styled from '@emotion/styled';
 
 type SidebarItemDeleteProps = {
-  active?: boolean,
-  onClick?: (e: React.SyntheticEvent<Element, Event>) => any | void,
-  children?: React.ReactNode,
-}
+  active?: boolean;
+  onClick?: (e: React.SyntheticEvent<Element, Event>) => any | void;
+  children?: React.ReactNode;
+};
 
 export const SidebarItemDelete = styled.div<SidebarItemDeleteProps>`
-    -moz-appearance: none;
-    -webkit-appearance: none;
-    display: block;
-    justify-self: flex-end;
-    transition: color 0.2s;
-    color: #c1c1c1;
-    margin-left: 0.5rem;
-    font-size: 14px;
-    padding: 5px;
-    border-radius: 5px;
-    &:hover {
-      color: #9a9a9a;
-      background: #e6e6e6;
-    }
+  -moz-appearance: none;
+  -webkit-appearance: none;
+  display: block;
+  justify-self: flex-end;
+  transition: color 0.2s;
+  color: #c1c1c1;
+  margin-left: 0.5rem;
+  font-size: 14px;
+  padding: 5px;
+  border-radius: 5px;
+  &:hover {
+    color: #9a9a9a;
+    background: #e6e6e6;
+  }
 `;
