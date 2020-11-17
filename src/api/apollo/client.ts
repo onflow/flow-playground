9bbda6b578e8d89a51981edcbd2f4f97380ff7be
Max Daunarovich <max.daunarovich@gmail.com>
2020-11-17 15:31:25 +0300

    Use more general PLAYGROUND_API environmental variable

--- src/api/apollo/client.ts
+++ src/api/apollo/client.ts
@@ -7,7 +7,7 @@ import localResolvers from "./resolvers";
 
 import { ResultType } from "./generated/graphql";
 
-const api = process.env.GRAPHQL_API;
+const api = process.env.PLAYGROUND_API
 
 const cache = new InMemoryCache();
 
@@ -15,7 +15,7 @@ const client = new ApolloClient({
   cache: cache,
   link: ApolloLink.from([
     new HttpLink({
-      uri: api,
+      uri: `${api}/query`,
       credentials: "include",
       fetch
     })
