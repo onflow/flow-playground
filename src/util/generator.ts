844ca117e46304748739f7fbf23756f250e2307b
Max Daunarovich <max.daunarovich@gmail.com>
2021-01-13 19:25:12 +0300

    More code generation magic

--- src/util/generator.ts
+++ src/util/generator.ts
@@ -1,5 +1,4 @@
 import prettier from 'prettier';
-
 export const getNameByAddress = (address: string) => {
   const addressBook: any = {
     '0x01': 'Alice',
@@ -59,19 +58,8 @@ export const generateAddressMap = (
   return '';
 };
 
-export const generateScriptCode = (template: string) => {
-  const imports = getImports(template);
-  const accountsCode = generateGetAccounts(imports);
-  const addressMapCode = generateAddressMap(imports);
-  let result = '\n';
-  result += accountsCode;
-  result += addressMapCode;
-
-  return result;
-};
-
 export const getArgumentsFromTemplate = (template: string) => {
-  const pattern = /(?:transaction\s*\()(.*)(?:\)\s*\{)|(?:fun main\()(.*)(?:\)\s*\{)/g;
+  const pattern = /(?:transaction\s*\()(.*)(?:\).*)|(?:fun main\()(.*)(?:\).*)/g;
   const result = pattern.exec(template);
 
   const match = result[1] || result[2];
@@ -85,28 +73,109 @@ export const getArgumentsFromTemplate = (template: string) => {
   return [];
 };
 
-export const generateArguments = (template: string) => {
-  // TODO: implement extraction of arguments
-  console.log(template);
+export const getDefaultValueForType = (type: string) => {
+  const values: { [index: string]: number | string } = {
+    Int: 1337,
+    Int8: 8,
+    Int16: 16,
+    Int32: 32,
+    UInt: 1337,
+    UInt8: 8,
+    UInt16: 16,
+    UInt32: 32,
+    UFix64: 1.0,
+    String: 'Hello',
+    Address: '0x0ae53cb6e3f42a79', // default address for FlowToken on Emulator
+  };
+
+  return values[type];
+};
+
+export const zipArguments = (
+  argumentsList: { name: string; type: string }[],
+) => {
+  return argumentsList.reduce(
+    (acc: any, item: { name: string; type: string }) => {
+      const defaultValue = getDefaultValueForType(item.type);
+      if (acc.length === 0) {
+        acc.push({
+          values: [defaultValue],
+          type: item.type,
+        });
+      } else {
+        const current = acc[acc.length - 1];
+        if (current.type === item.type) {
+          current.values.push(defaultValue);
+        } else {
+          acc.push({
+            values: [defaultValue],
+            type: item.type,
+          });
+        }
+      }
+
+      return acc;
+    },
+    [],
+  );
+};
+
+export const generateArgumentsCode = (argumentsList: any) => {
+  if (argumentsList.length > 0) {
+    let result = `const args = [
+      ${argumentsList.map((item: { values: any[]; type: string }) => {
+        return `[${item.values.join(',')}, types.${item.type}]`;
+      })}
+    ]`;
+    return result;
+  }
+
   return '';
 };
 
-export const replaceScriptTemplate = (
-  scriptName: string,
-  addressMap: string,
-  args: string,
-) => {
+/*
+  Script Generation Process
+  - Gather Data
+    - get list of imports
+    - get list of arguments
+    - get expected result type
+    - get list of "getAccount" calls, then add new addressed to account list
+
+  - Generate Code
+    - generate getContractAddress($name) from list of imports
+    - generate "getAccountAddress($name)" calls from account list
+    - generate argument creation from list of arguments
+      - provide meaningful default values
+      - add comment that those values shall be updated
+    - generate "executeScript" call
+    - generate basic expectancy test (no errors)
+    - generate mockup expectancy test based on result type
+* */
+export const replaceScriptTemplate = (scriptName: string, template: string) => {
+  const imports = getImports(template);
+  const argumentsList = getArgumentsFromTemplate(template);
+  const zippedArguments = zipArguments(argumentsList);
+  const argumentsCode = generateArgumentsCode(zippedArguments);
+
+  // const accounts = generateGetAccounts(imports);
+  const addressMap = generateAddressMap(imports);
+
   let result = `test("test template for ##SCRIPT-NAME##", async () => {
-      ##ADDRESS-MAP##
+      // ##ADDRESS-MAP##
       
       const code = await getScriptCode({
-        name: "##SCRIPT-NAME##", ##ADDRESS-MAP-CONDITIONAL##
+        name: "##SCRIPT-NAME##", 
+        // ##ADDRESS-MAP-CONDITIONAL##
       });
       
-      ##ARGUMENTS##
+      // ##GET-ACCOUNTS##
+      // ##ACCOUNTS-REPLACEMENT##
+      
+      // ##ARGUMENTS##
             
       const result = await executeScript({
-        code, ##ARGS-CONDITIONAL##
+        code, 
+        // ##ARGS-CONDITIONAL##
       });
       
       // Add your expectations here
@@ -115,20 +184,33 @@ export const replaceScriptTemplate = (
   `.replace(/##SCRIPT-NAME##/g, scriptName);
 
   if (addressMap.length > 0) {
-    result = result.replace(/##ADDRESS-MAP##/, addressMap);
-    result = result.replace(/##ADDRESS-MAP-CONDITIONAL##/, 'addressMap');
+    result = result.replace(/\/\/ ##ADDRESS-MAP##/, addressMap);
+    result = result.replace(/\/\/ ##ADDRESS-MAP-CONDITIONAL##/, 'addressMap');
   } else {
-    result = result.replace(/##ADDRESS-MAP##/, '');
-    result = result.replace(/##ADDRESS-MAP-CONDITIONAL##/, '');
+    result = result.replace(/\/\/ ##ADDRESS-MAP##/, '');
+    result = result.replace(/\/\/ ##ADDRESS-MAP-CONDITIONAL##/, '');
   }
 
-  if (args.length > 0) {
-    result = result.replace(/##ARGUMENTS##/, args);
-    result = result.replace(/##ARGS-CONDITIONAL##/, 'args');
+  if (argumentsCode.length > 0) {
+    result = result.replace(/\/\/ ##ARGUMENTS##/, argumentsCode);
+    result = result.replace(/\/\/ ##ARGS-CONDITIONAL##/, 'args');
   } else {
-    result = result.replace(/##ARGUMENTS##/, '');
-    result = result.replace(/##ARGS-CONDITIONAL##/, '');
+    result = result.replace(/\/\/ ##ARGUMENTS##/, '');
+    result = result.replace(/\/\/ ##ARGS-CONDITIONAL##/, '');
   }
 
   return prettier.format(result, { parser: 'babel' });
+};
+
+export const generateTransactionCode = (template: string) => {
+  const imports = getImports(template);
+  const accountsCode = generateGetAccounts(imports);
+  const addressMapCode = generateAddressMap(imports);
+  // TODO: add signers, but notify user that he shall update value here
+  // TODO: generate arguments, but mention, that user shall update values
+
+  let result = '\n';
+  result += accountsCode;
+  result += addressMapCode;
+  return result;
 };
--- test/util/generator.test.js
+++ test/util/generator.test.js
@@ -6,9 +6,45 @@ import {
   generateScriptCode,
   replaceScriptTemplate,
   getArgumentsFromTemplate,
+  zipArguments,
+  generateArgumentsCode,
 } from '../../src/util/generator';
 
 describe('Generator Related Unit Tests', () => {
+  test('properly zip arguments', () => {
+    const args = [
+      { type: 'Int', name: 'a' },
+      { type: 'Int', value: 'b' },
+      { type: 'String', value: 'name' },
+      { type: 'Address', value: 'recipient' },
+    ];
+    const zippedArgs = zipArguments(args);
+
+    expect(zippedArgs.length).toBe(3);
+    expect(zippedArgs[0].values.length).toBe(2);
+    expect(zippedArgs[0].type).toBe(args[0].type);
+
+    expect(zippedArgs[1].values.length).toBe(1);
+    expect(zippedArgs[1].type).toBe(args[2].type);
+
+    expect(zippedArgs[2].values.length).toBe(1);
+    expect(zippedArgs[2].type).toBe(args[3].type);
+  });
+
+  test('arguments code generation', () => {
+    const args = [
+      { type: 'Int', name: 'a' },
+      { type: 'Int', value: 'b' },
+      { type: 'String', value: 'name' },
+      { type: 'Address', value: 'recipient' },
+    ];
+    const zipped = zipArguments(args);
+    const code = generateArgumentsCode(zipped);
+    const result = code;
+    console.log(result);
+    expect(code.length).not.toBe(0);
+  });
+
   test('should find no imports in a script template', () => {
     const template = `
       pub fun main(){
@@ -92,43 +128,6 @@ describe('Generator Related Unit Tests', () => {
     console.log(getAccountsCode);
   });
 
-  test('should create proper code for script', () => {
-    const template = `
-      import First from 0x01
-      import Second from 0x02
-      import Third from 0x03
-      
-      pub fun main(){
-        console.log("Hello")
-      }
-    `;
-
-    const generatedCode = generateScriptCode(template);
-    console.log(generatedCode);
-  });
-
-  test('should create full proper code script test', () => {
-    const template = `
-      import First from 0x01
-      import Second from 0x02
-      import Third from 0x03
-      
-      pub fun main(){
-        console.log("Hello")
-      }
-    `;
-
-    const scriptName = 'script-01';
-    const addressMapCode = generateScriptCode(template);
-    const argumentsCode = generateArguments(template);
-    const generatedCode = replaceScriptTemplate(
-      scriptName,
-      addressMapCode,
-      argumentsCode,
-    );
-    console.log(generatedCode);
-  });
-
   test('should return list of arguments from script template', () => {
     const template = `
       pub fun main(a: Int, b: Int, recipient: Address) {
@@ -193,3 +192,25 @@ describe('Generator Related Unit Tests', () => {
     expect(args.length).toBe(0);
   });
 });
+
+describe("Generator - Scripts", ()=>{
+  test('should create proper code for all features', () => {
+    const template = `
+      import First from 0x01
+      import Second from 0x02
+      import Third from 0x03
+      
+      pub fun main(a: Int, b: Int, c: String, d:Address): String {
+        let Dave = getAccount(0x04)
+        return "Hello, Jest"
+      }
+    `;
+
+    const scriptName = 'script-01';
+    const generatedCode = replaceScriptTemplate(
+      scriptName,
+      template
+    );
+    console.log(generatedCode);
+  });
+})
