4fd1212ee97111038abced0331dabbeea074bfde
Max Daunarovich <max.daunarovich@gmail.com>
2021-01-18 00:01:24 +0300

    Update exporting code

--- src/components/ExportPopup.tsx
+++ src/components/ExportPopup.tsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import JSZip from 'jszip';
 import { saveAs } from 'file-saver';
+import prettier from 'prettier';
+import parserBabel from 'prettier/parser-babel';
 import { useProject } from 'providers/Project/projectHooks';
 
 import {
@@ -10,87 +12,130 @@ import {
   WhiteOverlay,
   SpaceBetween,
 } from 'components/Common';
+import { Project } from 'api/apollo/generated/graphql';
+import {
+  getContractName,
+  replaceContractTemplate,
+  replaceScriptTemplate,
+  replaceTransactionTemplate,
+} from '../util/generator';
 
 // TODO: should be replaced to "master" before merging or replaced with .env variable
-const BRANCH = "max/export-project-as-zip"
-const GENERATOR_ROOT = `https://raw.githubusercontent.com/onflow/flow-playground/${BRANCH}/project-generator`
+const BRANCH = 'max/export-project-as-zip';
+const GENERATOR_ROOT = `https://raw.githubusercontent.com/onflow/flow-playground/${BRANCH}/project-generator`;
 
-const getFile = async (filename: string) =>  {
-  const response = await fetch(`${GENERATOR_ROOT}/${filename}`)
-  const content = await response.text()
-  return content
-}
+const getFile = async (filename: string) => {
+  const response = await fetch(`${GENERATOR_ROOT}/${filename}`);
+  const content = await response.text();
+  return content;
+};
 
-const generateReadMe = async (id:"string") => {
-  const file = await getFile("files/README.md")
+const generateReadMe = async (id: 'string') => {
+  const file = await getFile('files/README.md');
   const readMeFile = file
-    .replace(/##PROJECT-ID##/g,`${id.toLowerCase()}`)
-    .replace(/##PROJECT-LINK##/g,`https://play.onflow.org/${id.toLowerCase()}`)
-  return readMeFile
-}
+    .replace(/##PROJECT-ID##/g, `${id.toLowerCase()}`)
+    .replace(
+      /##PROJECT-LINK##/g,
+      `https://play.onflow.org/${id.toLowerCase()}`,
+    );
+  return readMeFile;
+};
+
 const generatePackageConfig = async (projectName: string) => {
-  const file = await getFile("files/package.json")
-  const config = file
-    .replace(/##PROJECT-NAME##/g, projectName.toLowerCase())
-  return config
-}
-const generateTests = async (baseFolder: string) =>{
-  const base = await getFile("snippets/imports.js")
-  const content = base.replace(/##BASE-FOLDER##/, baseFolder)
+  const file = await getFile('files/package.json');
+  const config = file.replace(/##PROJECT-NAME##/g, projectName.toLowerCase());
+  return config;
+};
 
-  // TODO: inject deployment scripts
-  // TODO: inject transactions and scripts in specified order via #pragma-order tags
+const generateTests = async (baseFolder: string, project: Project) => {
+  const base = await getFile('snippets/imports.js');
+  let content = base.replace(/##BASE-FOLDER##/, baseFolder);
 
-  return content
-}
+  let deploymentTests = '';
+  for (let i = 0; i < project.accounts.length; i++) {
+    const account = project.accounts[i];
+    const address = `0x0${account.address.slice(-1)}`;
+    const code = account.draftCode;
+    const unitTest = replaceContractTemplate(address, code);
+    deploymentTests += unitTest;
+    deploymentTests += '\n';
+  }
+  content = content.replace(/\/\/ ##DEPLOYMENT-TESTS##/, deploymentTests);
+
+  let executionTests = '';
+  for (let i = 0; i < project.transactionTemplates.length; i++) {
+    const tx = project.transactionTemplates[i];
+    const unitTest = replaceTransactionTemplate(tx.title, tx.script);
+    executionTests += unitTest;
+    executionTests += '\n';
+  }
+
+  for (let i = 0; i < project.scriptTemplates.length; i++) {
+    const script = project.scriptTemplates[i];
+    const unitTest = replaceScriptTemplate(script.title, script.script);
+    executionTests += unitTest;
+    executionTests += '\n';
+  }
+  content = content.replace(
+    /\/\/ ##TRANSACTIONS-AND-SCRIPTS-TESTS##/,
+    executionTests,
+  );
+  return prettier.format(content, { parser: 'babel', plugins: [parserBabel] });
+};
 
 const ExportPopup: React.FC<{
   visible: boolean;
   triggerClose?: (e: React.SyntheticEvent) => any;
 }> = ({ visible, triggerClose }) => {
-  const [processing, setProcessing] = useState(false);
   const { project } = useProject();
+  const [processing, setProcessing] = useState(false);
+  const [projectName, setProjectName] = useState(`project-${project.id}`);
+  const [folderName, setFolderName] = useState('cadence');
 
   const createZip = async () => {
-
     const zip = new JSZip();
 
     // Create setup files
-    const readMeFile = await generateReadMe(project.id)
-    const packageConfig = await generatePackageConfig(`playground-project-${project.id.toLowerCase()}`)
-    const babelConfig = await getFile("files/babel.config.json")
-    const jestConfig = await getFile("files/package.json")
-    const testFile = await generateTests("cadence")
-    zip.file("test/README.md", readMeFile);
-    zip.file("test/package.json", packageConfig)
-    zip.file("test/babel.config.json", babelConfig)
-    zip.file("test/jest.config.json", jestConfig)
-    zip.file("test/index.test.js", testFile)
+    const readMeFile = await generateReadMe(project.id);
+    const packageConfig = await generatePackageConfig(
+      `playground-project-${project.id.toLowerCase()}`,
+    );
+    const babelConfig = await getFile('files/babel.config.json');
+    const jestConfig = await getFile('files/package.json');
+    const testFile = await generateTests(folderName, project);
+
+    zip.file('test/README.md', readMeFile);
+    zip.file('test/package.json', packageConfig);
+    zip.file('test/babel.config.json', babelConfig);
+    zip.file('test/jest.config.json', jestConfig);
+    zip.file('test/index.test.js', testFile);
 
     const { accounts, transactionTemplates, scriptTemplates } = project;
 
     for (let i = 0; i < accounts.length; i++) {
       const account = accounts[i];
-      const fileName = `cadence/contracts/contract-0${i}.cdc`;
+      const name = getContractName(account.draftCode);
+      const fileName = `cadence/contracts/${name}.cdc`;
       zip.file(fileName, account.draftCode);
     }
 
     for (let i = 0; i < transactionTemplates.length; i++) {
       const template = transactionTemplates[i];
-      const fileName = `cadence/transactions/tx-0${i}.cdc`;
+      const name = template.title;
+      const fileName = `cadence/transactions/${name}.cdc`;
       zip.file(fileName, template.script);
     }
 
     for (let i = 0; i < scriptTemplates.length; i++) {
       const template = scriptTemplates[i];
-      const fileName = `cadence/scripts/script-0${i}.cdc`;
+      const name = template.title;
+      const fileName = `cadence/scripts/${name}.cdc`;
       zip.file(fileName, template.script);
     }
 
     // Save everything as ZIP
     const projectFile = await zip.generateAsync({ type: 'blob' });
-    const projectName = `project-${project.id}.zip`;
-    saveAs(projectFile, projectName);
+    saveAs(projectFile, `${projectName}.zip`);
   };
 
   return (
@@ -98,7 +143,14 @@ const ExportPopup: React.FC<{
       <FullScreenContainer elevation={15}>
         <PopupContainer width="300px">
           <PopupHeader mb="20px">Export Project</PopupHeader>
-          <p>Here we will show popup information</p>
+          <input
+            value={projectName}
+            onChange={event => setProjectName(event.target.value)}
+          />
+          <input
+            value={folderName}
+            onChange={event => setFolderName(event.target.value)}
+          />
           {processing ? (
             <p>Processing...</p>
           ) : (
