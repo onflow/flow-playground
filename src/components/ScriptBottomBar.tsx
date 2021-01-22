23d5b0a4be1c3d03409c91ef734f6ed23ef99348
Mackenzie <remember.mackenzie@gmail.com>
2021-01-22 15:14:28 -0800

    Minimize result panels for developer happiness. (#106)
    * Minimize output panels.
    
    * centerize controls
    
    * Better shadow
    
    * Update src/components/Arguments/styles.tsx

--- src/components/ScriptBottomBar.tsx
+++ src/components/ScriptBottomBar.tsx
@@ -1,14 +1,15 @@
-import React, { useState, useEffect } from "react";
-import { ResultType } from "api/apollo/generated/graphql";
+import React, { useState, useEffect } from 'react';
+import { ResultType } from 'api/apollo/generated/graphql';
 import styled from '@emotion/styled';
 import useMousePosition from '../hooks/useMousePosition';
-import { RenderResponse } from "components/RenderResponse";
-import { Feedback as FeedbackRoot } from "layout/Feedback";
+import { RenderResponse } from 'components/RenderResponse';
+import { Feedback as FeedbackRoot } from 'layout/Feedback';
 import { ResizeHeading } from 'layout/Heading';
-import { ClearResults } from "./TransactionBottomBar";
+import { ClearResults } from './TransactionBottomBar';
+import { GoChevronDown, GoChevronUp } from 'react-icons/go';
 
-const RESULT_PANEL_MIN_HEIGHT = 180
-const PLAYGROUND_HEADER_HEIGHT = 75
+const RESULT_PANEL_MIN_HEIGHT = 80;
+const PLAYGROUND_HEADER_HEIGHT = 75;
 
 const FeedbackContainer = styled.div<{ height: number }>`
   display: flex;
@@ -19,18 +20,16 @@ const FeedbackContainer = styled.div<{ height: number }>`
   bottom: 0;
   left: 245px;
   background: white;
-	width: calc(100vw - 245px);
+  width: calc(100vw - 245px);
   height: ${(p) => p.height}px;
-	overflow-y: hidden;
+  overflow-y: hidden;
 `;
 
 const ScriptBottomBar: React.FC = () => {
-
-	const { x, y } = useMousePosition();
+  const { x, y } = useMousePosition();
 
   const [resultHeight, setResultHeight] = useState(140);
   const [isResizingResult, setIsResizingResult] = useState(false);
-
 
   const toggleResizingResult = (toggle: boolean) => {
     setIsResizingResult(toggle);
@@ -57,17 +56,23 @@ const ScriptBottomBar: React.FC = () => {
     };
   }, []);
 
-
   return (
-		<FeedbackContainer height={resultHeight}>
-    <FeedbackRoot>
-      <ResizeHeading onMouseDown={() => toggleResizingResult(true)}>
-        Script Results <ClearResults type={ResultType.Script} />
-				<div></div>
-      </ResizeHeading>
-      <RenderResponse resultType={ResultType.Script} />
-    </FeedbackRoot>
-		</FeedbackContainer>
+    <FeedbackContainer height={resultHeight}>
+      <FeedbackRoot>
+        <ResizeHeading onMouseDown={() => toggleResizingResult(true)}>
+          Script Results <ClearResults type={ResultType.Script} />
+          {resultHeight > 40 ? (
+            <GoChevronDown size="16px" onClick={() => setResultHeight(40)} />
+          ) : (
+            <GoChevronUp
+              size="16px"
+              onClick={() => setResultHeight(RESULT_PANEL_MIN_HEIGHT * 2)}
+            />
+          )}
+        </ResizeHeading>
+        <RenderResponse resultType={ResultType.Script} />
+      </FeedbackRoot>
+    </FeedbackContainer>
   );
 };
 
--- src/layout/Feedback.tsx
+++ src/layout/Feedback.tsx
@@ -1,11 +1,11 @@
-import styled from "@emotion/styled";
+import styled from '@emotion/styled';
 
 export const Feedback = styled.div<{ height?: number }>`
-  height: ${div => div.height || 180}px;
-	border-top: var(--gap) solid var(--key);
+  height: ${(div) => div.height}px;
+  border-top: var(--gap) solid var(--key);
   position: relative;
   display: flex;
   flex-direction: column;
   width: 100%;
-  min-height:80px;
+  min-height: 80px;
 `;
