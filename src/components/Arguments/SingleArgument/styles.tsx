63d313225f60e132156a222cc8a668e3e9e6915e
Max Daunarovich <max.daunarovich@gmail.com>
2021-01-27 16:36:20 +0300

    Add random name generator into popup. Add button to regenerate name. Adjust styles

--- src/components/Arguments/SingleArgument/styles.tsx
+++ src/components/Arguments/SingleArgument/styles.tsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import styled from "styled-components"
 
 interface InpubBlockProps {
@@ -8,6 +9,7 @@ export const InputBlock = styled.div<InpubBlockProps>`
   display: flex;
   flex-direction: column;
   margin-bottom: ${({mb = "0"}) => mb};
+  position: relative;
 `
 
 export const Label = styled.p`
@@ -46,3 +48,35 @@ export const Error = styled.p`
   font-size: 12px;
   color: red;
 `;
+
+export const InputIconContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  align-self: center;
+  right: 0.75em;
+  bottom: 0.9em;
+  cursor: pointer;
+  color: #888;
+  user-select: none;
+  
+  &:hover{
+    color: #333;
+  }
+`;
+
+
+type InputIconProps = {
+  onClick?: any,
+  icon: JSX.Element
+}
+
+export const InputIcon = (props: InputIconProps) => {
+  const { onClick, icon } = props
+  return (
+    <InputIconContainer onClick={onClick}>
+      {icon}
+    </InputIconContainer>
+  )
+}
