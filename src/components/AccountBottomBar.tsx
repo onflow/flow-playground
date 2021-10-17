2af1170346999abb3619adb8121ed73465e96c1d
Matt Chana <8943047+hichana@users.noreply.github.com>
2021-10-17 12:19:31 +0900

    cleanup

--- src/components/AccountBottomBar.tsx
+++ src/components/AccountBottomBar.tsx
@@ -279,29 +279,6 @@ const AccountState: React.FC<{
 
   const { storage, paths } = getStorageData(state)
 
-  // const storage: { [identifier: string]: string } = {};
-  // console.log("ACCOUNTSTATE STORAGE:", storage);
-  // const paths: { [identifier: string]: string } = {};
-  // console.log("ACCOUNTSTATE PATHS:", paths);
-
-  // const parsed = JSON.parse(state);
-  // // console.log("PARSED", parsed);
-  
-
-  // for (let key in parsed) {
-  //   if (!parsed.hasOwnProperty(key)) {
-  //     continue;
-  //   }
-
-  //   const tuple = key.split('\u001f')
-  //   const [domain, identifier] = tuple
-    
-
-  //   if (tuple.length === 2 && ['storage', 'public', 'private'].includes(domain)) {
-  //     storage[identifier] = parsed[key];
-  //     paths[identifier] = `/${domain}/${identifier}`
-  //   }
-  // }
   const identifiers = Object.keys(storage);
  
   const types: { [identifier: string]: string } = {};
