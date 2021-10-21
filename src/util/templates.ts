223546339561404130ef720bf3f13eb1083cdcec
hichana <hichana@gmail.com>
2021-10-21 15:41:20 +0900

    update script and tx templates with basic text

--- src/util/templates.ts
+++ src/util/templates.ts
@@ -1,61 +1,57 @@
 export const getInterpolatedTemplate = (type: string, contractOwner: string, path: string, contractResource: string, interfaces: string): any => {
   if (type === 'tx') {
-  return `// The 'borrowedCapability' constant borrows a capability from a public path
-    // for a given account. The capability is scoped to a contract its implemented
-    // contract-level interfaces. It was deployed in this playground by ${contractOwner}.
+  return `// TEMPLATE TRANSACTION
 
-    // Copy-paste 'borrowedCapability into a script that suits your needs. Make
-    // sure the 'address' parameter in 'getAccount()' is a valid account address.
+// Below, the 'borrowedCapability' constant borrows a capability from a public path
+// for a given account. The capability is scoped to a given contract and the implemented
+// interfaces within that contract. For the Capability you chose the contract was 
+// deployed by ${contractOwner} and implements these interfaces: ${interfaces}.
 
-    /*
-      ...
-    */
-  
+// Copy-paste 'borrowedCapability into a transaction that suits your needs. Make
+// sure the 'address' parameter in 'getAccount()' is a valid account address.
 
-    // Here's an example transaction that uses a capability scoped to a fungible token
-    // contract that implements the 'Balance' interface and returns the balance
-    // as a UFix64 type
-
-    /*
-      ...
-    */
-      `
+/*
+let borrowedCapability = getAccount(address)
+    .getCapability(${path})
+    .borrow<&${contractResource}{${interfaces}}>()
+    ?? panic("Could not borrow the capability")
+*/`
 
   } else if (type === 'script') {
-  return `// The 'borrowedCapability' constant borrows a capability from a public path
-    // for a given account. The capability is scoped to a contract its implemented
-    // contract-level interfaces. It was deployed in this playground by ${contractOwner}.
+  return `// TEMPLATE SCRIPT
 
-    // Copy-paste 'borrowedCapability into a script that suits your needs. Make
-    // sure the 'address' parameter in 'getAccount()' is a valid account address.
+// Below, the 'borrowedCapability' constant borrows a capability from a public path
+// for a given account. The capability is scoped to a given contract and the implemented
+// interfaces within that contract. For the Capability you chose the contract was 
+// deployed by ${contractOwner} and implements these interfaces: ${interfaces}.
 
-    /*
-    let borrowedCapability = getAccount(address)
-        .getCapability(${path})
-        .borrow<&${contractResource}{${interfaces}}>()
-        ?? panic("Could not borrow Balance capability")
-    */
-  
+// Copy-paste 'borrowedCapability into a script that suits your needs. Make
+// sure the 'address' parameter in 'getAccount()' is a valid account address.
 
-    // Here's an example script that uses a capability scoped to a fungible token
-    // contract that implements the 'Balance' interface and returns the balance
-    // as a UFix64 type
+/*
+let borrowedCapability = getAccount(address)
+    .getCapability(${path})
+    .borrow<&${contractResource}{${interfaces}}>()
+    ?? panic("Could not borrow the capability")
+*/
 
-    /*
-    import ExampleToken from 0x01
 
-      pub fun main(address: Address): UFix64 {
-          let account = getAccount(address)
+// Here's an example script that uses a capability scoped to a fungible token
+// contract that implements the 'Balance' interface and returns the balance
+// as a UFix64 type.
 
-          let vaultRef = account
-              .getCapability(/public/MainReceiver)
-              .borrow<&ExampleToken.Vault{ExampleToken.Balance}>()
-              ?? panic("Could not borrow Balance capability")
+import ExampleToken from 0x01
 
-          return vaultRef.balance
-      }
-      */
-      `
+  pub fun main(address: Address): UFix64 {
+      let account = getAccount(address)
+
+      let vaultRef = account
+          .getCapability(/public/MainReceiver)
+          .borrow<&ExampleToken.Vault{ExampleToken.Balance}>()
+          ?? panic("Could not borrow Balance capability")
+
+      return vaultRef.balance
+  }`
   }
 
 }
