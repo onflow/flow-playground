f425c24b8f27e0d54ec502c582d93b8d8ec2e063
Max Daunarovich <max.daunarovich@gmail.com>
2022-01-18 18:47:16 +0300

    Remove readme type

--- src/util/url.ts
+++ src/util/url.ts
@@ -11,6 +11,6 @@ export const getParams = (url: string): any =>  {
   },{})
 }
 
-export const scriptTypes = ["account", "tx", "script", "readme"]
+export const scriptTypes = ["account", "tx", "script"]
 
 export const LOCAL_PROJECT_ID = "local-project"
