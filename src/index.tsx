35523c17d3eddcf7429c74deb6391c529fc81a0d
Max Daunarovich <max.daunarovich@gmail.com>
2022-01-28 22:56:49 +0300

    Add comment to trigger snyk

--- src/index.tsx
+++ src/index.tsx
@@ -2,5 +2,7 @@ import * as React from "react"
 import * as ReactDOM from "react-dom"
 import App from "./App"
 
+// simple comment to re-trigger snyk
+
 const root = document.getElementById("root")
 ReactDOM.render(<App />, root)
