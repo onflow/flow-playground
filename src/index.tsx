c6745d079fc2d1d910ea2b79367ee9df04e828f5
Max Daunarovich <max.daunarovich@gmail.com>
2022-01-28 22:57:29 +0300

    Remove comment

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,5 @@ import * as React from "react"
 import * as ReactDOM from "react-dom"
 import App from "./App"
 
-// simple comment to re-trigger snyk
-
 const root = document.getElementById("root")
 ReactDOM.render(<App />, root)
