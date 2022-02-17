bb2aa3ce6693ea20becc520cec9515e1b21b4dcd
Max Daunarovich <max.daunarovich@gmail.com>
2022-02-17 16:05:56 +0300

    Redirect from empty project id

--- src/containers/Editor/index.tsx
+++ src/containers/Editor/index.tsx
@@ -3,10 +3,15 @@ import { ProjectProvider } from 'providers/Project';
 import EditorLayout from './layout';
 import { Base } from 'layout/Base';
 import { LOCAL_PROJECT_ID } from 'util/url';
+import { Redirect} from '@reach/router';
 
 const Playground: any = (props: any) => {
   const { projectId } = props;
   const isLocalProject = projectId === LOCAL_PROJECT_ID;
+
+  if (!projectId) {
+    return <Redirect noThrow to={`/${LOCAL_PROJECT_ID}`} />;
+  }
 
   return (
     <Base>
