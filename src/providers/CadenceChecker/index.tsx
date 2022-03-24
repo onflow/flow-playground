bc2cd338c47c1d83ae9aa535c047094e6b5418d6
Max Daunarovich <max.daunarovich@gmail.com>
2022-03-24 18:18:17 +0300

    Remove getCode dependency

--- src/providers/CadenceChecker/index.tsx
+++ src/providers/CadenceChecker/index.tsx
@@ -1,30 +1,11 @@
 import React, { createContext } from 'react';
 import useLanguageServer from '../../hooks/useLanguageServer';
-import { useProject } from 'providers/Project/projectHooks';
 
 export const CadenceCheckerContext: React.Context<any> = createContext(null);
 
 export default function CadenceChecker(props) {
-  const project = useProject();
-
   // Connect project to cadence checker hook
-  const cadenceChecker = useLanguageServer({
-    getCode: (address) => {
-      const { accounts } = project.project;
-
-      const number = parseInt(address, 16);
-      if (!number) {
-        return;
-      }
-
-      const index = number - 1;
-      if (index < 0 || index >= accounts.length) {
-        return;
-      }
-      let code = accounts[index].draftCode;
-      return code;
-    },
-  });
+  const cadenceChecker = useLanguageServer();
 
   // render
   const { children } = props;
