2b1b3a04fb2be52bbfd472029fa9df3c4babae6b
Gregor Gololicic <gregor.gololicic@dapperlabs.com>
2022-09-07 11:16:48 +0200

    filter out messages

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,35 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-
 import * as Sentry from '@sentry/react';
 import { BrowserTracing } from '@sentry/tracing';
-import { CaptureConsole as CaptureConsoleIntegration } from '@sentry/integrations';
 
 import App from './App';
 
 Sentry.init({
   dsn: process.env.SENTRY_DSN,
   integrations: [
-    new BrowserTracing(),
-    new CaptureConsoleIntegration({ levels: ['error'] }),
+    new BrowserTracing()
   ],
   tracesSampleRate: 1.0,
+  beforeSend(event, hint) {
+    const error = hint.originalException;
+
+    const ignoreErrors = [
+      /GraphQL/i, // filter out graphql errors
+      /Failed to fetch/i // filter out failed to fetch network errors
+    ]
+    
+    // filter out blacklisted errors
+    if (error && error.message) {
+      for (const filter in ignoreErrors) {
+        if (error.message.match(filter)) {
+          return null
+        }
+      }
+    }
+
+    return event;
+  },
 });
 
 const root = document.getElementById('root');
