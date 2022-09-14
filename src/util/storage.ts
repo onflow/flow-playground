4ad9a40f9310580921b7a4f484b76cc1e07cf4aa
Alex Serban <al.se@me.com>
2022-09-14 13:38:54 -0400

    Adapt to the new account storage format - Update storage display to use new api format - Add public/private tags - Remove template popup and functionality. Will not be part of the new playground

--- src/util/storage.ts
+++ src/util/storage.ts
@@ -1,3 +1,5 @@
+const RESOURCE_TYPES = ['Public', 'Private'];
+
 // take in the state for an account, return data for resources explorer
 export const getStorageData = (state: string = ''): any => {
   if (state === '') {
@@ -7,7 +9,6 @@ export const getStorageData = (state: string = ''): any => {
   const storage: { [identifier: string]: string } = {};
   const paths: { [identifier: string]: string } = {};
   const types: { [identifier: string]: string } = {};
-  const capabilities: { [identifier: string]: any } = {};
 
   const parsed = JSON.parse(state);
 
@@ -17,57 +18,18 @@ export const getStorageData = (state: string = ''): any => {
       continue;
     }
 
-    const tuple = key.split('\u001f');
-    const [domain, identifier] = tuple;
+    const domain = key.toLowerCase();
+    if (['Storage', 'Public', 'Private'].includes(key)) {
+      Object.keys(parsed[key] || {}).forEach((identifier) => {
+        storage[identifier] = parsed[key][identifier];
 
-    if (
-      tuple.length === 2 &&
-      ['storage', 'public', 'private'].includes(domain)
-    ) {
-      storage[identifier] = parsed[key];
-      const path = `/${domain}/${identifier}`;
-      paths[identifier] = path;
-      const storageItemType = parsed[key].value?.type || null;
-      types[identifier] = storageItemType;
-
-      if (storageItemType === 'Link') {
-        const borrowType = parsed[key].value.value.borrowType;
-        const borrowTypeSplit = borrowType.split('.');
-        const contractAcctId = borrowTypeSplit[1];
-        const contractAddr = `0x0${contractAcctId.substr(
-          contractAcctId.length - 1,
-        )}`;
-        const contract = borrowTypeSplit[2];
-        const resourcePart = borrowTypeSplit[3];
-        const resource = resourcePart.split('{')[0];
-
-        const rxp = /{([^}]+)}/g;
-
-        const foundInterfacesRegEx = rxp.exec(borrowType);
-
-        let interfaces: string[] = [];
-        if (foundInterfacesRegEx) {
-          const foundInterfaces = foundInterfacesRegEx[1];
-          const fullyQualifiedInterfaces = foundInterfaces.split(',');
-          fullyQualifiedInterfaces.map((fullyQualifiedInterface) => {
-            interfaces.push(
-              fullyQualifiedInterface.split('.')[2] +
-                '.' +
-                fullyQualifiedInterface.split('.')[3],
-            );
-          });
-        }
-
-        capabilities[identifier] = {
-          path: path,
-          contractAddr: contractAddr,
-          resourceContract: contract,
-          resource: resource,
-          contractImplementedInterfaces: interfaces,
-        };
-      }
+        const path = `/${domain}/${identifier}`;
+        paths[identifier] = path;
+        types[identifier] = RESOURCE_TYPES.includes(identifier)
+          ? 'Resource'
+          : 'Capability';
+      });
     }
   }
-
-  return { storage, paths, types, capabilities };
+  return { storage, paths, types };
 };
