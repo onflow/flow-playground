7115fa379882580f2fe3d9d64c1da8d78acc778c
Daniel Cheng <dcheng47@gmail.com>
2022-12-08 20:47:04 -0800

    linter

--- src/components/CadenceEditor/index.tsx
+++ src/components/CadenceEditor/index.tsx
@@ -5,7 +5,6 @@ import { useProject } from 'providers/Project/projectHooks';
 import configureCadence, { CADENCE_LANGUAGE_ID } from 'util/cadence';
 import { EditorContainer } from './components';
 import ControlPanel from './ControlPanel';
-import Notifications from './Notifications';
 import { EditorState } from './types';
 import { EntityType } from 'providers/Project';
 
