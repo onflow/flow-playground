bda923895fd6633173f708925a4f4fa42b5b5bf6
Daniel Cheng <dcheng47@gmail.com>
2023-01-17 14:52:01 -0800

    added hotjar

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,11 @@ import * as Sentry from '@sentry/react';
 import { BrowserTracing } from '@sentry/tracing';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-
+import { PLAYGROUND_HOTJAR_SITE_ID, HOTJAR_VERSION } from 'util/globalConstants';
 import App from './App';
+import Hotjar from '@hotjar/browser';
+
+Hotjar.init(PLAYGROUND_HOTJAR_SITE_ID, HOTJAR_VERSION);
 
 Sentry.init({
   dsn: process.env.SENTRY_DSN,
--- src/util/globalConstants.ts
+++ src/util/globalConstants.ts
@@ -4,3 +4,7 @@
 
 export const PLAYGROUND_GITHUB_ISSUES_URL =
   'https://github.com/onflow/flow-playground/issues/new/choose';
+
+export const PLAYGROUND_HOTJAR_SITE_ID = 3324806;
+
+export const HOTJAR_VERSION = 6;
