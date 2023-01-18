f05277df19dd07161a5e423e2c0802e292a69009
Daniel Cheng <dcheng47@gmail.com>
2023-01-17 18:15:26 -0800

    removed site id

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,12 @@ import { BrowserTracing } from '@sentry/tracing';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import {
-  PLAYGROUND_HOTJAR_SITE_ID,
   HOTJAR_VERSION,
 } from 'util/globalConstants';
 import App from './App';
 import Hotjar from '@hotjar/browser';
 
-Hotjar.init(PLAYGROUND_HOTJAR_SITE_ID, HOTJAR_VERSION);
+Hotjar.init(parseInt(process.env.HOTJAR_SITE_ID), HOTJAR_VERSION);
 
 Sentry.init({
   dsn: process.env.SENTRY_DSN,
