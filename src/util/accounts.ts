aaa51be5ab8d253f42a325526a1f7c0f825a6d64
Tom Haile <bradford.haile@gmail.com>
2023-09-01 11:56:48 -0500

    update account addresses to 5 ..9

--- src/util/accounts.ts
+++ src/util/accounts.ts
@@ -1,4 +1,4 @@
-const accountAddresses = ['0x01', '0x02', '0x03', '0x04', '0x05'];
+const accountAddresses = ['0x05', '0x06', '0x07', '0x08', '0x09'];
 export const storageMapByIndex = (index: number): string => {
   if (index < 0) return null;
   return accountAddresses[index];
