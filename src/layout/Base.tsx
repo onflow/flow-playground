ec1e8fe032f2943f5370f0b23c1699cb8156d4a5
Tom Haile <bradford.haile@gmail.com>
2023-09-05 17:00:26 -0500

    fix linting error

---
+++ src/layout/Base.tsx
@@ -0,0 +1,24 @@
+import styled from '@emotion/styled';
+import theme from '../theme';
+
+export const Base = styled.div`
+  --gap: 1px;
+  --key: ${theme.colors.border};
+  --bg: white;
+
+  position: absolute;
+  top: 0px;
+  right: 0px;
+  bottom: 0px;
+  left: 0px;
+  height: 100vh;
+  display: grid;
+  grid-gap: var(--gap) var(--gap);
+  grid-template-areas:
+    'header header'
+    'sidebar main';
+  grid-template-columns: 244px auto;
+  grid-template-rows: 112px auto;
+  background: var(--key);
+  overflow: hidden;
+`;
