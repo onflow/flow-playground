f8476cafded333d4c5387ad57a6867c00c33d30b
Alex Ni <12097569+nialexsan@users.noreply.github.com>
2024-09-12 17:45:44 -0400

    fix indices

--- src/util/accounts.ts
+++ src/util/accounts.ts
@@ -1,4 +1,4 @@
-const accountAddressIndexes = ['5', '6', '7', '8', '9'];
+const accountAddressIndexes = ['6', '7', '8', '9', 'a'];
 const accountAddresses = ['0x06', '0x07', '0x08', '0x09', '0x0a'];
 export const storageMapByIndex = (index: number): string => {
   if (index < 0) return null;
